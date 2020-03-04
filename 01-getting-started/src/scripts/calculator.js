e885dcaa5ebb40a043bb89fd76291760e46258e4
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2020-03-03 16:02:37 -0800

    Calculator update

---
+++ 01-getting-started/src/scripts/calculator.js
@@ -0,0 +1,28 @@
+const calculator = {
+
+
+    add: (num1, num2) => {
+        let sum = num1 + num2;
+        return sum;
+    },
+
+    substract: (num1, num2) => {
+        let substr = num1 - num2;
+        return substr;
+
+    },
+
+    multiply: (num1, num2) => {
+        let mult = num1*num2;
+        return mult;
+    },
+
+    divide: (num1, num2) => {
+        let div = num1/num2;
+        return div;
+    },
+
+   
+};
+
+export default calculator;
