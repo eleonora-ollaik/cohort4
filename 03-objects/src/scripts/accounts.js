ee7db7c4e813980968aaddbcf91574a1982b1c09
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2020-04-06 10:47:34 -0600

    updating code

--- 03-objects/src/scripts/accounts.js
+++ 03-objects/src/scripts/accounts.js
@@ -1,24 +1,47 @@
 class Account {
-    constructor (accountName, startingBalance) {
+    constructor(accountName, startingBalance) {
         this.name = accountName;
         this.balance = startingBalance;
     }
-    
-    deposit (amount) {
+
+    deposit(amount) {
         this.balance += Number(amount);
         return this.balance;
     }
 
-    withdraw (amount) {
+    withdraw(amount) {
         this.balance -= Number(amount);
         return this.balance;
     }
 
-    balance () {
+    balance() {
         return this.balance;
     }
 };
 
+class Card {
+
+    constructor (card) {
+    const div = document.createElement('div');
+        let li = document.createElement('li');
+        li.appendChild(document.createTextNode(card));
+        div.appendChild(li);
+
+        // let card = li.appendChild(document.createTextNode(`${accountName}: $${startingBalance}`));
+        // div.appendChild(li);
+
+
+        const delBtn = document.createElement('button');
+        const delBtnText = document.createTextNode('Delete');
+        delBtn.appendChild(delBtnText);
+        li.appendChild(delBtn);
+        document.querySelector('ul').appendChild(div);
+
+        li.classList.add("form-control");;
+        delBtn.setAttribute("class", "btn btn-outline-secondary");
+
+    }
+}
 
 
 // 130C
@@ -28,82 +51,60 @@ class AccountControl {
         this.accArr = [];
     }
 
-addAccount(accountName, startingBalance) {
-    this.accArr.push(new Account(accountName, startingBalance));
+    addAccount(accountName, startingBalance) {
+        this.accArr.push(new Account(accountName, startingBalance));
+        new Card(document.createTextNode(`${this.name}: $${this.balances}`));
 
-    const div1 = document.createElement('div');
-    const div2 = document.createElement('div');
-
-    
-    let li = document.createElement('li');
-
-    
-    li.appendChild(document.createTextNode(`${accountName}: $${startingBalance}`));
-    div1.appendChild(div2);
-    div2.appendChild(li);
-    document.querySelector('ul').appendChild(li);
-    
-
- 
-
-    const delBtn = document.createElement('button');
-		const delBtnText = document.createTextNode('Delete');
-		delBtn.appendChild(delBtnText);
-        
-        div1.setAttribute('class','input-group mb-3');
-        div2.classList.add('input-group-append')
-        li.classList.add("form-control");;
-        div2.appendChild(delBtn);
-        delBtn.setAttribute("class", "btn btn-outline-secondary");
-       
-}
-
-
-
-totalBalance() {
-    let total = 0;
-    for (let i = 0; i < this.accArr.length; i++) {
-        total += Number(this.accArr[i].balance);
+      
     }
-    return `$${total}`;
-}
 
-highestBalance() {
-    let highBal = 0;
-    let highName;
-    for (let i = 0; i < this.accArr.length; i++) {
-        if (this.accArr[i].balance > highBal) {
-            let currentName = this.accArr[i].name;
-            highBal = Number(this.accArr[i].balance);
-            highName = currentName;
+
+
+    totalBalance() {
+        let total = 0;
+        for (let i = 0; i < this.accArr.length; i++) {
+            total += Number(this.accArr[i].balance);
         }
+        return `$${total}`;
     }
-    return `${highName}: $${highBal}`;
-}
 
-lowestBalance() {
-    let lowestBal = Number.POSITIVE_INFINITY;
-    let lowName;
-    for (let i = this.accArr.length-1; i>=0; i--) {
+    highestBalance() {
+        let highBal = 0;
+        let highName;
+        for (let i = 0; i < this.accArr.length; i++) {
+            if (this.accArr[i].balance > highBal) {
+                let currentName = this.accArr[i].name;
+                highBal = Number(this.accArr[i].balance);
+                highName = currentName;
+            }
+        }
+        return `${highName}: $${highBal}`;
+    }
+
+    lowestBalance() {
+        let lowestBal = Number.POSITIVE_INFINITY;
+        let lowName;
+        for (let i = this.accArr.length - 1; i >= 0; i--) {
             let low = Number(this.accArr[i].balance);
             let currentName = this.accArr[i].name;
             if (low < lowestBal) {
-            lowestBal = low;
-            lowName = currentName;
+                lowestBal = low;
+                lowName = currentName;
+            }
+        }
+        return `${lowName}: $${lowestBal}`;
+    }
+
+    deleteAccount(accountName) {
+        for (let i = 0; i < this.accArr.length; i++) {
+            if (accountName === this.accArr[i].name) {
+                this.accArr.splice(i, 1)
+            }
         }
     }
-    return `${lowName}: $${lowestBal}`;
-}
-
-deleteAccount (accountName) {
-    for (let i=0; i< this.accArr.length; i++) {
-        if (accountName === this.accArr[i].name) {
-            this.accArr.splice(i,1)
-        }
-    }
-}
 
 }
 
 
-export {Account, AccountControl};
+
+export { Account, AccountControl };
--- 03-objects/src/scripts/index.js
+++ 03-objects/src/scripts/index.js
@@ -4,6 +4,8 @@ let accountManager = new AccountControl();
 
 let addAccBtn = document.getElementById('addAccountBtn');
 
+//Creating new account
+
 addAccBtn.addEventListener('click', () => {
     let accName = document.getElementById('accName').value;
     let startingBalance = document.getElementById('startBal').value;
@@ -11,9 +13,25 @@ addAccBtn.addEventListener('click', () => {
     // console.log(startingBalance);
 
 accountManager.addAccount(accName, startingBalance);
-// alert(`Account '${accName}' with the balance of $${startingBalance} was created!`);
-accName ='';
-startingBalance='';
+// accName.value ='';
+// startingBalance.value='';
     console.log(accountManager.accArr);
 })
 
+
+// Depositing Money
+
+let depAmount = document.getElementById('depositAmount').value;
+let depBtn = document.getElementById('depositBtn');
+
+
+depBtn.addEventListener('click', () => {
+    
+})
+document.body.addEventListener('click', e => {
+    if (e.target.nodeName === 'LI') {
+        var string = e.target.textContent;
+        var numbers = string.match(/\d+/g).map(Number);
+        console.log(numbers)
+        accountManager.accArr[i].deposit(depAmount)
+}});
