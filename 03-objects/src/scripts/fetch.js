8457f64ed7697d93d04328e9df99ea967ce0d176
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2020-04-30 19:12:45 -0600

    Fetch exercise

---
+++ 03-objects/src/scripts/fetch.js
@@ -0,0 +1,85 @@
+
+
+const functions = {
+
+    url: 'https://jsonplaceholder.typicode.com/users',
+
+    getFirstName(data) {
+        let firstName = data[0].name;
+        return firstName;
+    },
+
+    getAllFirstNames(data) {
+        return data.map((d, i, x) => d.name);
+    },
+
+    showDelayProblem() {
+        console.log('One');
+        setTimeout(() => {
+            console.log('Two');
+        }, 1 * 1000);
+        console.log('Three');
+    },
+
+    async showDelaySolution() {
+        try {
+            console.log('One');
+            const value = await                 // Simulate fetch
+                new Promise(
+                    (resolve, reject) =>
+                        setTimeout(() => resolve("Two"),
+                            1 * 1000));
+            // Now that we have the value we can use it.
+            console.log(value);
+            console.log('Three');
+        } catch (error) {
+            console.log(error);
+        }
+    },
+
+    async getUsers() {
+        try {
+            const response = await fetch(functions.url);
+            const data = await response.json();
+            return data;
+        } catch (error) {
+            console.error('Error:', error);
+            throw (error);
+        }
+    },
+
+    async workWithData() {
+        const data = await functions.getUsers();
+        console.log(functions.getFirstName(data));
+        console.log(functions.getAllFirstNames(data));
+    },
+
+    async  postData(url = '', data = {}) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: 'POST',     // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors',       // no-cors, *cors, same-origin
+            cache: 'no-cache',  // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: 'same-origin', // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json'
+                // 'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            redirect: 'follow',         // manual, *follow, error
+            referrer: 'no-referrer',    // no-referrer, *client
+            body: JSON.stringify(data)  // body data type must match "Content-Type" header
+        })
+        const json = await response.json();   // parses JSON response into native JavaScript objects
+        json.status = response.status;
+        json.statusText = response.statusText;
+        return json;
+    },
+}
+
+
+
+
+
+
+
+export default functions;
