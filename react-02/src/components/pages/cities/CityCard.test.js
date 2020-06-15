602cd661600831e3d129c9b63ffc99141e77249f
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2020-06-14 23:06:13 -0600

    added test for CityCard

---
+++ react-02/src/components/pages/cities/CityCard.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import {render, screen, fireEvent} from '@testing-library/react'
+import CityCard from './CityCard';
+
+const mockPopIncrease = jest.fn();
+const mockPopDecrease = jest.fn();
+const mockDelete = jest.fn();
+
+test ('the card renders', () => {
+
+    const card1 = 
+    {key:'k1', name:'Paris', latitude: 48.85341, longtitude: 2.3488, population: 2148000};
+
+    const {getByText} =
+    render(<CityCard card ={card1} onMoveIn = {mockPopIncrease} onMoveOut = {mockPopDecrease} onDelete = {mockDelete} />);
+
+    expect(getByText(/City name/i)).toHaveTextContent('Paris');
+    expect(getByText(/City latitudee/i)).toHaveTextContent('48.85341');
+    expect(getByText(/City longitude/i)).toHaveTextContent('2.3488');
+    expect(getByText(/City populatiom/i)).toHaveTextContent('2148000');
+
+    screen.getByText(/move in/i)
+    screen.getByText(/move out/i)
+    screen.getByText(/delete/i)
+
+    expect(mockPopIncrease.mock.calls.length).toBe(0);
+    click ('Move In');
+    expect(mockPopIncrease.mock.calls.length).toBe(1);
+
+    expect(mockPopDecrease.mock.calls.length).toBe(0);
+    click ('Move Out');
+    expect(mockPopDecrease.mock.calls.length).toBe(1);
+
+    expect(mockDelete.mock.calls.length).toBe(0);
+    click ('Delete');
+    expect(mockDelete.mock.calls.length).toBe(1);
+
+
+})
+
+function click (txt) {
+    fireEvent.click(
+        screen.getByText(txt)
+    );
+  }
+  
