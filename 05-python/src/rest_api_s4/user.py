772c2543bbc5a0fc6563e36bcedd3f138041f4f1
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2020-08-06 10:48:10 -0600

    python comp update

--- 05-python/src/rest_api_s4/user.py
+++
@@ -1,9 +0,0 @@
-
-class User:
-    def __init__(self, _id, username, password):
-        self.id = _id #id is a python keyword and we don't want to use it as a var name, therefore we're  using _id 
-        self.username = username
-        self.password = password
-
-
-
